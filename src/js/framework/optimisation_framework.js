a2bd1124cc2c0deec7c47d05d2b4e64b4e635570
Confoederatio <htmlprogrammingbn@gmail.com>
2025-05-22 09:50:28 +0100

    Update optimisation_framework.js

--- src/js/framework/optimisation_framework.js
+++ src/js/framework/optimisation_framework.js
@@ -20,12 +20,7 @@
     createContextMenuIndex({
       config_key: "entity_keyframes",
       namespace: "EntityKeyframe"
-    }); //This is seemingly glitched for no reason
-    config.flattened_entity_keyframes = dumbFlattenObject(config.entity_keyframes);
-
-    config.all_entity_keyframes = getAllEntityKeyframes();
-    config.all_entity_keyframe_keys = getAllEntityKeyframes({ return_keys: true });
-    config.entity_keyframes_lowest_order = getEntityKeyframesLowestOrder();
+    });
 
     //GROUP ACTIONS
     //Set .all_group_actions; .all_group_actions_keys
