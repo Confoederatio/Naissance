f944568742894c17727d83deef4532b89f5fcacf
Confoederatio <htmlprogrammingbn@gmail.com>
2025-12-12 10:25:20 +0000

    updated versioning

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ let { performance } = require("perf_hooks");
 
 //Metadata - Title
 let latest_fps = 0;
-let naissance_version = "1.5.2b Tanganyika";
+let naissance_version = "1.6b Falkland";
 let title_update_interval;
 let win;
 
