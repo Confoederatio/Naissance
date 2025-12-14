8359630cc9eeecd2cd107367e92c8ca7acabea15
Confoederatio <htmlprogrammingbn@gmail.com>
2025-12-14 09:32:05 +0000

    added autobuilder system

---
+++ autobuild.bat
@@ -0,0 +1,10 @@
+@echo off
+title Naissance (Binary Builder)
+echo [Naissance HGIS] Binary is being compiled for Windows ..
+call npm run build:win -- --config.directories.output="./_dist/windows" > _dist/output_windows.txt
+echo [Naissance HGIS] Binary is being compiled for MacOS ..
+call npm run build:mac -- --config.directories.output="./_dist/mac" > _dist/output_mac.txt
+echo [Naissance HGIS] Binary is being compiled for Linux ..
+call npm run build:linux -- --config.directories.output="./_dist/linux" > _dist/output_linux.txt
+echo [Naissance HGIS] Binary compilation process finished. Please check _dist/ for logs.
+pause
